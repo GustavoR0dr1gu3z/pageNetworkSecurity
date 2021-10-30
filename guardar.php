df7f52c62f552e7f6775f66c89d150ffe34c8d54
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2021-10-30 00:07:27 -0500

    signup listo

--- guardar.php
+++ guardar.php
@@ -5,26 +5,11 @@
 	$nombre = $_POST['nombre'];
 	$correo = $_POST['correo'];
 	$telefono = $_POST['telefono'];
-	$estado_civil = $_POST['estado_civil'];
-	$hermanos = isset($_POST['hermanos']) ? $_POST['hermanos'] : 0;
-	$intereses = isset($_POST['intereses']) ? $_POST['intereses'] : null;
 	
-	$arrayintereses = null;
-	
-	$num_array = count($intereses);
 	$contador = 0;
+
 	
-	if($num_array>0){
-		foreach ($intereses as $key => $value) {
-			if ($contador != $num_array-1)
-			$arrayintereses .= $value.' ';
-			else
-			$arrayintereses .= $value;
-			$contador++;
-		}
-	}
-	
-	$sql = "INSERT INTO universitarios (nombre, correo, telefono, estado_civil, hermanos, intereses) VALUES ('$nombre', '$correo', '$telefono', '$estado_civil', '$hermanos', '$arrayintereses')";
+	$sql = "INSERT INTO Users (nombre, correo, telefono) VALUES ('$nombre', '$correo', '$telefono')";
 	$resultado = $mysqli->query($sql);
 	
 ?>
@@ -35,18 +20,19 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<link href="css/bootstrap.min.css" rel="stylesheet">
 		<link href="css/bootstrap-theme.css" rel="stylesheet">
+		<link rel="stylesheet" href="css/styles.css">
 		<script src="js/jquery-3.1.1.min.js"></script>
 		<script src="js/bootstrap.min.js"></script>	
 	</head>
 	
 	<body>
 		<div class="container">
-			<div class="row">
+			<div class="rowMio3">
 				<div class="row" style="text-align:center">
 					<?php if($resultado) { ?>
-						<h3>REGISTRO GUARDADO</h3>
+						<h3 class="h3Mio2">REGISTRO GUARDADO</h3>
 						<?php } else { ?>
-						<h3>ERROR AL GUARDAR</h3>
+						<h3 class="h3Mio2">ERROR AL GUARDAR</h3>
 					<?php } ?>
 					
 					<a href="index.php" class="btn btn-primary">Regresar</a>
